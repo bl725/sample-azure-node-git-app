62773d2ac7e6278a487b9a214cf2e9de111fcf96
billl <N/A>
2026-01-06 08:37:43 -0800

    add comments to server.js + modify display message

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+/*
+This is a simple Node.js application that can be deployed to Azure App Service using Git.
+It uses the Express framework to create a basic web server.
+*/
+
 const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Node.js application deployed to Azure App Service using Git');
+    res.send('Node.js application deployed to Azure App Service using Git.');
 });
 
 const port = process.env.PORT || 3000;
